46b41b518f01aca30e8118ae302098aa206b7b21
sonu shahuji <sonushahuji4@gmail.com>
2018-12-19 12:16:06 +0530

    Double Circular linked list commit version 1.1 Adding 1)Insert Data at Beg.. 2)Insert Data from End 3)Delete Data 4)Delete Data at Beg.. 5)Delete Data from End 6)Serach Data

--- Double_Circular_Linked_List/DoubleCircularLinkedList.py
+++ Double_Circular_Linked_List/DoubleCircularLinkedList.py
@@ -25,6 +25,104 @@ class DoubleCircularLinkedList:
             cur.next=self.start
             self.start.prev=cur
 
+    def insertdatabeg(self,data):
+        if self.start is None:
+            new_node = Node(data)
+            self.start = new_node
+            self.start.next = self.start
+            self.start.prev = self.start
+        else:
+            cur=self.start
+            while cur.next != self.start:
+                cur=cur.next
+            if cur.next == self.start:
+                new_node=Node(data)
+                cur.next=new_node
+                new_node.next=self.start
+                self.start.prev=new_node
+                new_node.prev=cur
+                self.start=new_node
+
+    def insertdataend(self,data):
+        if self.start is None:
+            new_node = Node(data)
+            self.start = new_node
+            self.start.next = self.start
+            self.start.prev = self.start
+        else:
+            cur = self.start
+            while cur.next != self.start:
+                cur = cur.next
+            new_node = Node(data)
+            cur.next = new_node
+            new_node.prev = cur
+            cur = new_node
+            cur.next = self.start
+            self.start.prev = cur
+
+    def deletedata(self,data):
+        if self.start is None:
+            print("Database is Null")
+        else:
+            if self.start and self.start.data == data:
+                self.start=None
+            else:
+                cur = self.start
+                while cur and cur.data != data:
+                    temp=cur
+                    cur = cur.next
+                if cur.data == data:
+                    temp.next=cur.next
+                    cur.next.prev=temp
+                    cur.next=None
+                    cur.prev=None
+
+
+    def deletedatabeg(self):
+        if self.start is None:
+            print("Database is Null")
+        else:
+            cur=self.start
+            if self.start.next == self.start and self.start.prev == self.start:
+                self.start=None
+            else:
+                while cur.next != self.start:
+                    cur=cur.next
+                if cur.next == self.start:
+                    temp=self.start
+                    self.start=temp.next
+                    temp.next=None
+                    temp.prev=None
+                    self.start.prev=cur
+                    cur.next=self.start
+
+    def deletedataend(self):
+        if self.start is None:
+            print("Database is Null")
+        elif self.start.next == self.start:
+            self.start=None
+        else:
+            cur=self.start
+            while cur.next != self.start:
+                temp=cur
+                cur=cur.next
+            if cur.next == self.start:
+                cur.next=None
+                cur.prev=None
+                temp.next=self.start
+                self.start.prev=temp
+
+    def serachdata(self,data):
+        if self.start is None:
+            print("Database is Null")
+        else:
+            cur=self.start
+            count=1
+            while cur and cur.data != data:
+                count+=1
+                cur=cur.next
+            if cur.data == data:
+                print(cur.data,"was found at location",count)
     def displaydata(self):
         if self.start is None:
             print("Database is Null")
@@ -38,6 +136,13 @@ class DoubleCircularLinkedList:
                     break
 
 dc=DoubleCircularLinkedList()
+dc.insertdata(723)
+dc.insertdata(75)
+dc.insertdata(3247)
+dc.insertdata(72)
+dc.insertdata(73)
 dc.insertdata(7)
-dc.insertdata(10)
+dc.displaydata()
+print()
+dc.serachdata(75)
 dc.displaydata()
