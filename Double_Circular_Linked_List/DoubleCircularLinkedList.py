6cee4727d4545bed390c4ee8808c7daa5c21993a
sonu shahuji <sonushahuji4@gmail.com>
2018-12-19 22:33:20 +0530

    Double Circular linked list commit version 1.2 Adding 1)Insert Data at Specific Location

--- Double_Circular_Linked_List/DoubleCircularLinkedList.py
+++ Double_Circular_Linked_List/DoubleCircularLinkedList.py
@@ -60,6 +60,34 @@ class DoubleCircularLinkedList:
             cur.next = self.start
             self.start.prev = cur
 
+    def insertdataatspecificlocation(self,data,location):
+        if self.start is None:
+            print("Database is Null")
+        else:
+            cur=self.start
+            if location == 1:
+                while cur.next != self.start:
+                    cur = cur.next
+                if cur.next == self.start:
+                    new_node = Node(data)
+                    cur.next = new_node
+                    new_node.prev = cur
+                    new_node.next = self.start
+                    self.start.prev = new_node
+                    self.start = new_node
+            else:
+                while location > 1:
+                    temp = cur
+                    cur = cur.next
+                    print(cur.data)
+                    location -= 1
+                if location == 1:
+                    new_node = Node(data)
+                    cur.prev = new_node
+                    new_node.next = cur
+                    temp.next = new_node
+                    new_node.prev = temp
+
     def deletedata(self,data):
         if self.start is None:
             print("Database is Null")
@@ -123,6 +151,8 @@ class DoubleCircularLinkedList:
                 cur=cur.next
             if cur.data == data:
                 print(cur.data,"was found at location",count)
+
+
     def displaydata(self):
         if self.start is None:
             print("Database is Null")
@@ -144,5 +174,5 @@ dc.insertdata(73)
 dc.insertdata(7)
 dc.displaydata()
 print()
-dc.serachdata(75)
+dc.insertdataatspecificlocation(777777,1)
 dc.displaydata()
