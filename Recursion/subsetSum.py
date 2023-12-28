fbb1a572b2769f6dbfb0560fd5c8ae36df61fa37
Sonu Shahuji <sonushahuji4@gmail.com>
2023-12-28 11:49:34 +0530

    Total subset sum

--- Recursion/subsetSum.py
+++ Recursion/subsetSum.py
@@ -17,4 +17,22 @@ subsetSum(0,0)
 subsetSumArr.sort()
 return subsetSumArr
 
+
+# To Find Total subset sum without creating an array
+
+arr = [2,3]
+n = len(arr)
+sumTotal = 0
+
+def subsetSum(i, total):
+  if i == n:
+    return total
+    
+  leftSum = subsetSum(i+1, total + arr[i]) # Pick
+  rightSum = subsetSum(i+1, total + 0) # not Pick
+  return leftSum + rightSum
+
+return subsetSum(0,0)
+
+
  
