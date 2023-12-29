e8fc0b4c3702f2f8d1afa49ab5ef1174dfc16958
Sonu Shahuji <sonushahuji4@gmail.com>
2023-12-29 13:42:47 +0530

    combinationSum one template

---
+++ Recursion/combinationSumOne.py
@@ -0,0 +1,41 @@
+# Link : https://leetcode.com/problems/combination-sum/description/
+
+# Approach One
+ans = []
+n = len(candidates)
+def combinationSum(i,target,arr):
+    if i == n:
+        if target == 0:
+            ans.append(arr.copy())
+        return
+    
+    if candidates[i] <= target:
+        arr.append(candidates[i])
+        combinationSum(i,target - candidates[i], arr)
+        arr.pop()
+    combinationSum(i+1, target, arr)
+  
+combinationSum(0,target,[])
+return ans
+
+# Approach Two
+ans = []
+n = len(candidates)
+def combinationSum(index, target, arr):
+  if index == n:
+    if target == 0:
+      ans.append(arr.copy())
+    return
+
+  for i in range(index, n):
+    if candidates[i] <= target:
+      arr.append(candidates[i])
+      combinationSum(i, target - candidates[i], arr)
+      arr.pop()
+combinationSum(0, target, [])
+return ans
+      
+      
+      
+  
+
