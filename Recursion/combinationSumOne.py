48fbff605e2f77488d5f92f8bd622a9aeb74f188
Sonu Shahuji <sonushahuji4@gmail.com>
2023-12-30 12:36:46 +0530

    removed if condition

--- Recursion/combinationSumOne.py
+++ Recursion/combinationSumOne.py
@@ -22,16 +22,15 @@ return ans
 ans = []
 n = len(candidates)
 def combinationSum(index, target, arr):
-  if index == n:
     if target == 0:
-      ans.append(arr.copy())
-    return
-
-  for i in range(index, n):
-    if candidates[i] <= target:
-      arr.append(candidates[i])
-      combinationSum(i, target - candidates[i], arr)
-      arr.pop()
+        ans.append(arr.copy())
+        return
+        
+    for i in range(index, n):
+        if candidates[i] <= target:
+            arr.append(candidates[i])
+            combinationSum(i, target - candidates[i], arr)
+            arr.pop()
 combinationSum(0, target, [])
 return ans
       
