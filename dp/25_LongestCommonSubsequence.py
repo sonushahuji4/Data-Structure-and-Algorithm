03ce84099a84b331534df51830b46e53bc8f2ed4
Sonu Shahuji <sonushahuji4@gmail.com>
2024-01-01 06:56:56 +0530

    count and string for the logest common subsquence

--- dp/25_LongestCommonSubsequence.py
+++ dp/25_LongestCommonSubsequence.py
@@ -1,5 +1,6 @@
 # Link : https://leetcode.com/problems/longest-common-subsequence/description/
 
+# This will return count of longest common subsquence
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
 
@@ -23,4 +24,32 @@ class Solution:
             return dp[i][j]
             
         return solve(n-1, m-1)
+
+# This will return string of longest common subsquence
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+
+        n = len(text1)
+        m = len(text2)
+
+        dp = [[-1]*(m+1) for _ in range(n)]
+
+        def solve(i,j):
+            if i < 0 or j < 0: return ""
+
+            if dp[i][j] != -1: return dp[i][j]
+
+            if text1[i] == text2[j]:
+                return text1[i] + solve(i-1, j-1)
+
+            leftMatch = solve(i-1, j)
+            rightMatch = solve(i, j-1)
+
+            dp[i][j] = max(leftMatch, rightMatch)
+            return dp[i][j]
+
+        ans = solve(n-1, m-1)
+        ans = ans[::-1]
+        return ans
+        
         
