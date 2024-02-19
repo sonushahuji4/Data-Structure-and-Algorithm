20db1bdfdf713a032412a08b42e64daa1193ec17
Sonu Shahuji <sonushahuji4@gmail.com>
2024-02-19 09:40:45 +0530

    Create 9_maximizeBeauty.py

---
+++ heap/9_maximizeBeauty.py
@@ -0,0 +1,20 @@
+# Quesiton (Asked In Google) in 2nd round
+
+  # A limited-time sale is currently ongoing for toys. Each day ith is represented by two parameters: A[i] denotes the sale end time, and B[i] represents the beauty of that day's offering.
+  # The sale starts at time T = 0, and it takes one unit of time to purchase one toy. However, a toy can only be bought if the current time T is less than the sale end time A[i].
+  # The goal is to maximize the sum of the beauty of the purchased toys.
+
+    Examples
+    
+    Input
+      A[i] = 3 1 3 2 3
+      B[i] = 6 5 3 1 9
+
+    Output : 20
+
+    Input
+      A[i] = 1 3 3 3 5 5 5 8
+      B[i] = 5 2 7 1 4 3 8 1
+
+    Output : 28
+
