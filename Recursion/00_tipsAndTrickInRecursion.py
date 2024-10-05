cacf2247b01d5be0f2228066314c5d79d23485ff
Sonu Shahuji <sonushahuji4@gmail.com>
2024-10-05 22:22:36 +0530

    Create 00_tipsAndTrickInRecursion.py

---
+++ Recursion/00_tipsAndTrickInRecursion.py
@@ -0,0 +1,2 @@
+# 1. Whenever question taks about choice or decision that means you can apply recursion there.
+# 2. Always try to build recursion tree, while solving recursion problems since it solve 90% of your problems.
