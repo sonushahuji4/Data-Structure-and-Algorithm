02964fda27a7c8058909b5a58e97bd6c65652178
Sonu Shahuji <sonushahuji4@gmail.com>
2024-10-08 10:11:39 +0530

    Create 01_LargestElement.py

---
+++ Array/01_LargestElement.py
@@ -0,0 +1,15 @@
+# Given an array, we have to find the largest element in the array.
+
+# Approach One
+arr = [1,4,6,3,8,9,0,3]
+sort.arr()
+return arr[-1]
+
+# Approach Two
+
+arr = [1,4,6,3,8,9,0,3]
+largestElement = arr[0]
+for i in range(len(arr):
+    if arr[i] > largestElement:
+        largestElement = arr[i]
+return largestElement
