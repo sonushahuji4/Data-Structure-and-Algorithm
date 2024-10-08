a24e7edf55c4a4cfbcb55777320fb30d61ccf16e
Sonu Shahuji <sonushahuji4@gmail.com>
2024-10-08 11:24:16 +0530

    Update 05_MoveZeroes.py

--- Array/05_MoveZeroes.py
+++ Array/05_MoveZeroes.py
@@ -18,3 +18,41 @@ class Solution:
             else: 
                 nums[i] = nonZeroArr[i]
 
+
+# Approach Two
+
+n = len(nums)
+        j = 0  # Pointer for the position to place non-zero elements
+
+        # Move non-zero elements to the front
+        for i in range(n):
+            if nums[i] != 0:
+                nums[j] = nums[i]
+                j += 1
+
+        # Fill the rest of the array with zeros
+        for i in range(j, n):
+            nums[i] = 0
+
+
+# Approach Third
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        n = len(nums)
+        j = -1
+
+        # Find the first zero
+        for i in range(n):
+            if nums[i] == 0:
+                j = i
+                break
+        
+        # If a zero was found, proceed to move non-zero elements
+        if j != -1:
+            for i in range(j + 1, n):
+                if nums[i] != 0:
+                    nums[j], nums[i] = nums[i], nums[j]
+                    j += 1
+        
+
