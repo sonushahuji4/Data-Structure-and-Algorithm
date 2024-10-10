ed768327b88cf179cbe83cf150675d6605ba69ad
Sonu Shahuji <sonushahuji4@gmail.com>
2024-10-10 12:29:28 +0530

    Update 00_HeapQuestionIdentification.py

--- heap/00_HeapQuestionIdentification.py
+++ heap/00_HeapQuestionIdentification.py
@@ -1,5 +1,5 @@
 # 1. Look for the key cooncept "K" and it's normaly sorting questions
-# 2. Look for the smallest or the largest or the top in the question
+# 2. Look for the smallest or the largest or the top or lowest or closest in the question
 # 3. Point 1 and point, if you find the combination of above the it is a heap question.
 # 4. There are two types of heap
 # 5. Min Heap (if K + Largest then choose min heap)
