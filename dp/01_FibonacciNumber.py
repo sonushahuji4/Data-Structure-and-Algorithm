1c0ebcd2e8eb74de20054370c4e9e6d80735353d
Sonu Shahuji <sonushahuji4@gmail.com>
2024-10-12 00:32:08 +0530

    Update 01_FibonacciNumber.py

--- dp/01_FibonacciNumber.py
+++ dp/01_FibonacciNumber.py
@@ -25,3 +25,23 @@ class Solution:
         dp[n] = leftCnt + rightCnt
         return dp[n]
       return fibo(n)
+
+
+    # Tabulation
+    dp = [-1] * (n+1)
+    dp[0] = 0
+    dp[1] = 1
+    for i in range(2,n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+
+
+    # Space optimization
+    previous2 = 0
+    previous1 = 1
+    for i in range(2,n+1):
+        current = previous1 + previous2
+        previous2 = previous1
+        previous1 = current
+    return previous1
+        
+        
