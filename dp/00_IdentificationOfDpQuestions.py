52a29d30a0bb09a5ac28a31abd8ecad94d7bd5fc
Sonu Shahuji <sonushahuji4@gmail.com>
2024-10-12 00:12:34 +0530

    Create 00_IdentificationOfDpQuestions.py

---
+++ dp/00_IdentificationOfDpQuestions.py
@@ -0,0 +1,9 @@
+# 1. Count the number of ways
+# 2. There are multiple ways to do this, find min or max kindly of statments
+# 3. Try all possible ways like count, best way etc
+# 4. Best way to approach dp or recursion problems
+#    Try to represent the problems in terms of index
+#    Do all possible ways on that index according to the problem statement
+#    If quesitons say count all ways then "sum up all staffs"
+#    If questions says min of all staff then find min
+#    if questions says max of all staff then find max
