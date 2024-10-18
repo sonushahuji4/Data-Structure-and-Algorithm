da2e45898ffe7a0ea75342256d24811968307052
Sonu Shahuji <sonushahuji4@gmail.com>
2024-10-18 08:46:39 +0530

    Update 41_Longest Increasing Subsequence.py

--- dp/41_Longest Increasing Subsequence.py
+++ dp/41_Longest Increasing Subsequence.py
@@ -4,7 +4,6 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         
-
         # Approach One (Recursion)
         def lis(ind, prev, n):
             if ind == n: return 0
@@ -37,3 +36,23 @@ class Solution:
             dp[ind][prev] = lisLen
             return dp[ind][prev]
         return lis(0,-1)
+
+        # Approach Three Tabulation
+
+        n = len(nums)
+        # Initialize dp table with all 0's (since LIS length cannot be negative)
+        dp = [[0]*(n+1) for _ in range(n+1)]
+        
+        for ind in range(n-1, -1, -1):
+            for prev in range(ind - 1, -2, -1): # Include prev == -1
+            
+                # Case 1: Not picking the current element
+                lisLen = 0 + dp[ind+1][prev+1]
+
+                # Case 2: Picking the current element if it's greater than the previous element
+                if prev == -1 or nums[ind] > nums[prev]:
+                    lisLen = max(lisLen, 1 + dp[ind + 1][ind+1])
+                
+                # Store the result
+                dp[ind][prev+1] = lisLen
+        return dp[0][0]
