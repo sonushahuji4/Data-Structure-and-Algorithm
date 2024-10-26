4ce4961c35ea71febb5781ee6f84a75f46429eb3
Sonu Shahuji <sonushahuji4@gmail.com>
2024-10-26 12:24:54 +0530

    Update 02_DesignGraphWithShortestPathCalculator.py

--- graph/Dijkstra/02_DesignGraphWithShortestPathCalculator.py
+++ graph/Dijkstra/02_DesignGraphWithShortestPathCalculator.py
@@ -1,5 +1,6 @@
  # Link : https://leetcode.com/problems/design-graph-with-shortest-path-calculator/
 
+# Approach One
 from collections import defaultdict
 from heapq import heappush, heappop
 
@@ -30,6 +31,50 @@ class Graph:
         if dist[node2] == float('inf'): return -1
         return dist[node2]
 
+        
+
+
+# Your Graph object will be instantiated and called as such:
+# obj = Graph(n, edges)
+# obj.addEdge(edge)
+# param_2 = obj.shortestPath(node1,node2)
+
+
+
+# Approach Two
+
+from collections import defaultdict
+import heapq
+
+class Graph:
+
+    def __init__(self, n: int, edges: List[List[int]]):
+        self.n = n
+        self.graph = defaultdict(list)
+        for source, destination, weight in edges:
+            self.graph[source].append((destination, weight))
+
+    def addEdge(self, edge: List[int]) -> None:
+        source, destination, weight = edge
+        self.graph[source].append((destination, weight))
+       
+
+    def shortestPath(self, node1: int, node2: int) -> int:
+        distances = {node: float('inf') for node in range(self.n)}
+        distances[node1] = 0
+        minHeapQ = [(0, node1)]
+        while minHeapQ:
+            weight, vertex = heapq.heappop(minHeapQ)
+            if weight > distances[vertex]: continue
+
+            for neighbour, cost in self.graph[vertex]:
+                distance = weight + cost
+                if distance < distances[neighbour]:
+                    distances[neighbour] = distance
+                    heapq.heappush(minHeapQ,(distance,neighbour))
+        if distances[node2] == float('inf'): return -1
+        return distances[node2]
+
 
         
 
@@ -38,3 +83,4 @@ class Graph:
 # obj = Graph(n, edges)
 # obj.addEdge(edge)
 # param_2 = obj.shortestPath(node1,node2)
+
