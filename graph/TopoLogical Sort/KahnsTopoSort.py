bf7e0fb7c2bb09750c5958fced116812893eb3fd
Sonu Shahuji <sonushahuji4@gmail.com>
2024-10-28 09:04:06 +0530

    Update KahnsTopoSort.py

--- graph/TopoLogical Sort/KahnsTopoSort.py
+++ graph/TopoLogical Sort/KahnsTopoSort.py
@@ -1,3 +1,49 @@
+# Template One
+
+from collections import defaultdict
+from queue import Queue
+
+class KahnsTopgSortAlgo:
+    def __init__(self, n, edges):
+        self.n = n
+        self.edges = edges
+        self.graph = defaultdict(list)
+        self.inDegree = defaultdict(int)
+        self.inDegreeQ = Queue()
+        
+        self.buildGraph()
+        self.calculateIndegrees()
+        self.initializeQwithZeroDegree()
+    
+    def buildGraph(self):
+        for (source, destination) in self.arr:
+            graph[source].append(destination)
+    
+    def calculateIndegrees(self):
+        for node in range(self.n):
+            for neighbor in self.graph[node]:
+                self.inDegree[neighbor] += 1
+    
+    def initializeQwithZeroDegree(self):
+        for node in range(self.n):
+            if self.inDegree[node] == 0:
+                self.inDegreeQ.put(node)
+    
+    def topoSort(self):
+        topoSort = []
+        while not inDegreeQ.empty():
+            node = self.inDegreeQ.get()
+            topoSort.append(node)
+            for neighbor in self.graph[node]:
+                self.inDegree[neighbor] -= 1
+                if self.inDegree[neighbor] == 0:
+                    self.inDegreeQ.put(neighbor)
+        return topoSort
+
+
+
+# Template Two
+
 from collections import defaultdict
 from queue import Queue
 
@@ -30,3 +76,6 @@ while not inDegreeQ.empty():
 
 # Return the topological sorting of the graph
 return topoSort
+
+
+
