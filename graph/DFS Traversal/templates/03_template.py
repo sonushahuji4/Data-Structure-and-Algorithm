c33e9707f8563a6471e6850f4f76948002fc10e0
Sonu Shahuji <sonushahuji4@gmail.com>
2024-10-31 22:54:35 +0530

    Create 03_template.py

---
+++ graph/DFS Traversal/templates/03_template.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+
+class DFS:
+    def __init__(self, n):
+        self.visited = [False]*n
+        self.n = n
+    
+    def dfs(self, node: int, parent: int, graph: List[List[int]]) -> bool:
+        self.visited[node] = True # Mark the node as visited
+        for neighbour in graph[node]:
+            if neighbour == parent: # Skip the parent to avoid false positive
+                continue
+            if self.visited[neighbour]: # Cycle found
+                return True
+            if not self.visited[neighbour]: Recursively visit neighbours
+                if self.dfs(neighbour, node, graph):
+                    return True
+        return False
+        
+class Solution:
+    # Function to detect cycle in an undirected graph.
+    def isCycle(self, V: int, adj: List[List[int]]) -> bool:
+        
+        dfsInstance = DFS(V)
+        for node in range(V):
+            if not dfsInstance.visited[node] and dfsInstance.dfs(node, -1, adj): 
+                return True
+        return False
