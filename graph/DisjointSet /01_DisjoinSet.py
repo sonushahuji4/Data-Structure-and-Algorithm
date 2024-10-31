3cd20f75b630ab3d097fea9b646619475249ece3
Sonu Shahuji <sonushahuji4@gmail.com>
2024-11-01 00:11:37 +0530

    Update 01_DisjoinSet.py

--- graph/DisjointSet /01_DisjoinSet.py
+++ graph/DisjointSet /01_DisjoinSet.py
@@ -1,3 +1,12 @@
+# The primary use of disjoint sets is to address the connectivity between the components of a network. 
+# The “network“ here can be a computer network or a social network. 
+# For instance, we can use a disjoint set to determine if two people share a common ancestor.
+
+# The find function finds the root node of a given vertex.
+# The union function unions two vertices and makes their root nodes the same.
+
+
+
 class UnionFindBySize:
     def __init__(self, n):
         # Initialize parent array with each element as its own parent
