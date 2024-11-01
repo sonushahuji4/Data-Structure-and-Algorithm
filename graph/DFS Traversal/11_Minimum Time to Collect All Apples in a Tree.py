63fdadc700a722d30c7ee8a2bf5769946db0e3e0
Sonu Shahuji <sonushahuji4@gmail.com>
2024-11-01 19:49:12 +0530

    Create 11_Minimum Time to Collect All Apples in a Tree.py

---
+++ graph/DFS Traversal/11_Minimum Time to Collect All Apples in a Tree.py
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/minimum-time-to-collect-all-apples-in-a-tree/description/
+
+from collections import defaultdict
+
+class DFS:
+    def __init__(self, n):
+        self.visited = [False]*n
+        self.total = 0
+    
+    def dfs(self, node: int, graph: List[List[int]], hasApple: List[bool]):
+        self.visited[node] = True
+        isSubTreeHasApple = False
+       
+        for neighbour in graph[node]:
+            if not self.visited[neighbour]:
+                if self.dfs(neighbour, graph, hasApple):
+                    self.total += 2
+                    isSubTreeHasApple = True
+
+        return hasApple[node] or isSubTreeHasApple
+
+class Solution:
+
+    def buildGraph(self, n: int, edges: List[List[int]]) -> List[List[int]]:
+        graph = defaultdict(list)
+        for u,v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+        return graph
+
+    def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
+        graph = self.buildGraph(n, edges)
+        dfsInstance = DFS(n)
+        dfsInstance.dfs(0,graph, hasApple)
+        return dfsInstance.total
