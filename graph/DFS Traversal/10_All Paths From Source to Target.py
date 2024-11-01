7e3744d0b8be2b60cbc0d824faddfb406ca8f519
Sonu Shahuji <sonushahuji4@gmail.com>
2024-11-01 12:38:33 +0530

    Create 10_All Paths From Source to Target.py

---
+++ graph/DFS Traversal/10_All Paths From Source to Target.py
@@ -0,0 +1,33 @@
+# https://leetcode.com/problems/all-paths-from-source-to-target/description/
+
+from collections import defaultdict
+
+class DFS:
+    def __init__(self,n):
+        self.visited = [False] * n
+        self.n = n
+    
+    def dfs(self, node, destination, graph):
+        if node == destination: return 1
+        self.visited[node] = True
+        pathCnt = 0
+        for neighbour in graph[node]:
+            if not self.visited[neighbour]:
+                pathCnt += self.dfs(neighbour, destination, graph)
+        self.visited[node] = False
+        return pathCnt
+
+class Solution:
+    def buildGraph(self, edges, n):
+        graph = defaultdict(list)
+        for node in range(n):
+            for neighbour in edges[node]:
+                graph[node].append(neighbour)
+        return graph
+
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        n = len(graph)
+        newGraph = self.buildGraph(graph,n)
+        dfsInstance = DFS(n)
+        cnt = dfsInstance.dfs(0, n-1, newGraph)
+        return cnt
