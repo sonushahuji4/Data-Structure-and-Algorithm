67d0253db7e2cd57023df912de86579187e2fa3e
Sonu Shahuji <sonushahuji4@gmail.com>
2025-07-18 14:14:49 +0530

    Iterative  Binary Serach Approach

--- Binary Search/01_binarySearch.py
+++ Binary Search/01_binarySearch.py
@@ -1,6 +1,7 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
 
+        # Recurssive Approach
         def binarySearch(low,high):
             if low > high: return -1
             mid = low + (high - low)//2
@@ -11,6 +12,17 @@ class Solution:
                 return binarySearch(low, mid - 1)
                 
         return binarySearch(0, len(nums)-1)
+
+        # Iterative Approach
+        low, high = 0, len(nums) - 1
+        while low <= high:
+            mid = low + (high - low)//2
+            if nums[mid] == target: return mid
+            elif nums[mid] > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return -1 
             
 
         
